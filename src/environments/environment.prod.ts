65bddd7edf9959f5f317e7c8d74506ff7e0e1996
Rodrigo Kamada <rodrigo@kamada.com.br>
2021-11-15 21:53:05 -0300

    Changing the URL production.

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,6 +3,6 @@ export const environment = {
   auth0: {
     domain: 'dev-5tf99p7c.us.auth0.com',
     clientId: 'GBPB42qhMWCtvrwGmYxvm5cbHXU68nzG',
-    redirectUri: 'http://localhost:4200/profile',
+    redirectUri: 'https://rodrigokamada.github.io/angular-auth0/profile',
   },
 };
